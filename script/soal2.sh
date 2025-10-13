82021d85e7f34a2f917964db250e2be2390147cb
ninduyi <anindyanabilah2@gmail.com>
2025-10-13 20:47:01 +0700

    soal 2 & 3

---
+++ script/soal2.sh
@@ -0,0 +1,4 @@
+# Di node Eonwe
+iptables -t nat -A POSTROUTING -o eth3 -j MASQUERADE -s 192.214.0.0/16
+
+
---
+++ script/soal3.sh
@@ -0,0 +1,2 @@
+# Di node lainnya (kecuali Eonwe)
+echo nameserver 192.168.122.1 > /etc/resolv.conf
